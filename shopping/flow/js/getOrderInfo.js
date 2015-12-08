d37c81178c1615ae7544f729dc4c31dd4c9f61e9
jidandan <jidandan@163.com>
2015-12-08 16:05:15 +0800

    *)修改js

--- shopping/flow/js/getOrderInfo.js
+++ shopping/flow/js/getOrderInfo.js
@@ -3,11 +3,11 @@ $(function(){
     $(".save").click(function(){
     $('.mask').fadeOut(100);
     $('.collect').slideUp(200);
-    $("#consignee-list").append('<li class="ui-switchable-panel ui-switchable-panel-selected" selected="selected"><div class="consignee-item"><span limit="3">姬单单 北京</span><b></b></div><div class="addr-detail"><span class="addr-name" limit="6" title="姬单单">姬单单</span><span class="addr-info" limit="45">北京 朝阳区 四环到五环之间 北京市朝阳区望京福码大厦A座18层</span><span class="addr-tel">132****7353</span></div><div class="op-btns"><a href="javascript:;" class="ftx-05 setdefault-consignee hide">设为默认地址</a><a href="javascript:;" class="ftx-05 edit-consignee onclick="use_NewConsignee()"">编辑</a><a href="javascript:;" class="ftx-05 del-consignee hide">删除</a></div></li>')
+    $("#consignee-list").append('<li class="ui-switchable-panel"><div class="consignee-item"><span>姬单单 北京</span><b></b></div><div class="addr-detail"><span class="addr-name" title="姬单单">姬单单</span><span class="addr-info">北京 朝阳区 四环到五环之间 北京市朝阳区望京福码大厦A座18层</span><span class="addr-tel">132****7353</span></div><div class="op-btns" style="display:none;"><a href="javascript:;" class="ftx-05 setdefault-consignee">设为默认地址</a><a href="javascript:;" class="ftx-05 edit-consignee" onclick="use_NewConsignee()">编辑</a><a href="javascript:;" class="ftx-05 del-consignee">删除</a></div></li>')
     });
     //设置默认地址背景颜色以及文字显示与隐藏
     $(".op-btns").hide();
-    $(".ui-switchable-panel").mousemove(function(){
+    $(".ui-switchable-panel").live("mousemove",function(){
         $(this).addClass("li_hover");
         $(this).find(".op-btns").show();
         if($(this).find(".consignee-item").hasClass("item-selected")){
@@ -16,13 +16,13 @@ $(function(){
             $(this).find(".op-btns .setdefault-consignee").html("设置默认地址");
         }
     });
-    $(".ui-switchable-panel").mouseout(function(){
+    $(".ui-switchable-panel").live("mouseout",function(){
         $(this).removeClass("li_hover");
         $(this).find(".op-btns").hide();
     });
 	
     //收货人信息 
-    $(".consignee-item").click(function(){
+    $(".consignee-item").live("click",function(){
         if($(this).hasClass("item-selected")){
             //$(this).siblings(".op-btns").find(".setdefault-consignee").html("");
         }else{
@@ -33,7 +33,7 @@ $(function(){
     });	
 	
     //点击设置默认地址
-    $(".setdefault-consignee").click(function(){
+    $(".setdefault-consignee").live("click",function(){
         if($(this).parent().siblings(".consignee-item").hasClass("item-selected")){
         }else{
             $(this).parent().parent().siblings(".ui-switchable-panel").find(".consignee-item").removeClass("item-selected");
@@ -45,7 +45,7 @@ $(function(){
 //		alert("这是编辑");
     });
     //点击删除
-    $(".del-consignee").click(function(){
+    $(".del-consignee").live("click",function(){
         $(this).parent("div").parent("li").remove();
     });
 	
