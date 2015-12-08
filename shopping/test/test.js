f00b92511185b5307a6ae8c5418e9edce253fed8
chenyl <15001209007@163.com>
2015-12-08 17:16:35 +0800

    +)增加商品价钱

--- shopping/test/test.js
+++ shopping/test/test.js
@@ -22,4 +22,29 @@ function showGoodsInfo(){
     });  
 }
 
+/**
+ * 商品添加到购物车
+ * @returns {undefined}
+ */
+function addToCart(){
+    var service = {};  
+    service.inter_num = "0050";   
+    service.servicecode = "1001";
+    service.goodid = "1";
+    service.uerid = "1";
+    service = JSON.stringify(service);
+    
+    var send_url = rooturl + "/../webapi/index.php";
+    apiSendAjax(send_url, service, true, function (status, data) {
+        if(status == 0){
+            console.log(data);
+            $("#jsonRecode").html("success!" + "<br/>"); 
+            $("#jsonRecode").html(JSON.stringify(data));
+        } else {
+            console.log(data);
+            $("#jsonRecode").html("error!" + "<br/>"); 
+            $("#jsonRecode").html(JSON.stringify(data));
+        }
+    });  
+}
 
--- webapi/shopping/index.php
+++ webapi/shopping/index.php
@@ -15,11 +15,12 @@ function dealCartFlow($body){
     $servicecode  = $body['servicecode']; 
     switch($servicecode){
         case "1000"://商品展示 
+            /** 商品展示  -  业务处理 start */
             $sql_GoodIds = "select goods_id from goods";
             $_goodids = $mysqliObj->get_all($sql_GoodIds);
             
             foreach($_goodids as $key => $_goodid){
-               $sql_retrieveGood = "select goods_id, goods_name, goods_number, goods_weight,goods_desc from goods where goods_id=" . $_goodid["goods_id"]; 
+               $sql_retrieveGood = "select goods_id, market_price, shop_price, goods_name, goods_number, goods_weight,goods_desc from goods where goods_id=" . $_goodid["goods_id"]; 
                $sql_retrieveGoodImgList = "select gimg.good_img, gimg.master "
                 . "from goods as gd join goods_img gimg on gd.goods_id = gimg.goods_id where gd.goods_id=" . $_goodid["goods_id"];
                $shopData  = $mysqliObj->real_get($sql_retrieveGood);   
@@ -32,7 +33,13 @@ function dealCartFlow($body){
                     break;
                 }
             }
+            /** 商品展示  -  业务处理 end */
+            break;
+        case "1001"://加入购物车
+            /** 加入购物车  -  业务处理 start */
             
+            
+            /** 加入购物车  -  业务处理 end */
             break;
     }
     
