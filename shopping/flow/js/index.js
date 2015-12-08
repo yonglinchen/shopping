34a31d1ca274899bb2227ff2caf3e189eccbd488
jidandan <jidandan@163.com>
2015-12-08 18:03:08 +0800

    *)修改js

--- shopping/flow/js/index.js
+++ shopping/flow/js/index.js
@@ -12,11 +12,13 @@ $(function(){
     var send_url = rooturl + "/../webapi/index.php";
     apiSendAjax(send_url, service, true, function (status, data) {
         if(status == 0){
+            //alert(data.data[0].images[0].good_img);
             $(".dginfo_h2").text(data.data[0].goods_name);
             $(".dginfo_weight").text(data.data[0].goods_weight);
             $(".shop_price").text(data.data[0].shop_price);
             $(".market_price").text(data.data[0].market_price);
             $(".goodnum").val(data.data[0].goods_number);
+            $(".default_img").attr("src",data.data[0].images[0].good_img);
         } else {
             console.log(data);            
         }
